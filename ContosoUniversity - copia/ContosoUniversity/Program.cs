21710b9d169ce90e297c4c8b5aacaa07d959cefa
ismaelFiguera <ismaelfiguera112@gnail.com>
2025-04-11 17:46:13 +0200

    world

--- ContosoUniversity - copia/ContosoUniversity/Program.cs
+++ ContosoUniversity - copia/ContosoUniversity/Program.cs
@@ -1,20 +1,41 @@
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+ServerVersion version = MySqlServerVersion.AutoDetect(connectionString);
+
+
+builder.Services.AddDbContext<SchoolContext>(options =>
+                options.UseMySql(connectionString, version));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.UseExceptionHandler("/Home/Error");
+
+    var dbContext = scope.ServiceProvider.GetRequiredService<SchoolContext>();
+    dbContext.Database.EnsureDeleted();
+    dbContext.Database.EnsureCreated();
 }
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+    }
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
+
+
 
 app.MapControllerRoute(
     name: "default",
